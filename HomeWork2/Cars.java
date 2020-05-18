2137e8d0171f945a1f7f3258b6d8923ea23ed21f
SamarinEvgenii <57903146+SamarinEvgenii@users.noreply.github.com>
2020-05-18 21:59:48 +0400

    HomeWork-2

---
+++ HomeWork2/Cars.java
@@ -0,0 +1,24 @@
+public class Cars {
+    private int year;
+    private int hp;
+    private int model;
+
+    public Cars(int year, int hp, int model) {
+        this.year = year;
+        this.hp = hp;
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+}
